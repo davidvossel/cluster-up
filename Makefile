77ee373aba996f176e248fe3c2086a045c4841b2
David Vossel <davidvossel@gmail.com>
2019-05-28 15:34:28 -0400

    add make targets and default configs
    Signed-off-by: David Vossel <davidvossel@gmail.com>

---
+++ Makefile
@@ -0,0 +1,9 @@
+cluster-up:
+	./cluster/up.sh
+
+cluster-down:
+	./cluster/down.sh
+
+.PHONY: \
+	cluster-up \
+	cluster-down 
---
+++ hack/config.sh
@@ -0,0 +1,14 @@
+unset binaries docker_images docker_prefix master_ip network_provider kubeconfig manifest_docker_prefix 
+
+KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-${PROVIDER}}
+
+source ${KUBEVIRT_PATH}hack/config-default.sh
+
+# Allow different providers to override default config values
+test -f "${KUBEVIRT_PATH}hack/config-provider-${KUBEVIRT_PROVIDER}.sh" && source ${KUBEVIRT_PATH}hack/config-provider-${KUBEVIRT_PROVIDER}.sh
+
+# Let devs override any default variables, to avoid needing
+# to change the version controlled config-default.sh file
+test -f "${KUBEVIRT_PATH}hack/config-local.sh" && source ${KUBEVIRT_PATH}hack/config-local.sh
+
+export binaries docker_images docker_prefix master_ip network_provider kubeconfig manifest_docker_prefix 
